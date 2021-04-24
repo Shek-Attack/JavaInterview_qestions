4d6281e44e0c323e495c7a714dc1803c9fe923e1
Noorl <ab427902@dal.ca>
2021-04-24 17:37:53 -0400

    over riding static method

--- src/OOPs/StaticOverRide.java
+++ src/OOPs/StaticOverRide.java
@@ -2,21 +2,33 @@ package OOPs;
 
 class A{
     public static void staticMethod(){
-        System.out.println("printing static method");
+        System.out.println("printing static method A");
 
     }
     protected void method1(){
         System.out.println("Printing A method");
+        //System.out.println("Print 1 static ");
 
     }
 }
 
 class B extends A{
 
+    public  static void staticMethod(){
+        System.out.println("print static method 2");
+    }
     @Override
     protected void method1() {
         System.out.println("Printing B method");
 
+
+    }
+}
+
+class C extends B{
+
+    protected void method1(){
+        System.out.println("Printing C from method 2");
     }
 }
 
@@ -25,8 +37,14 @@ public class StaticOverRide {
         B b = new B();
         b.method1();
 
-        A.staticMethod();
-        B.staticMethod();
+        C c = new C();
+        c.method1();
+
+        System.out.println("============================================");
+
+        A.staticMethod(); // call from class name.
+        B.staticMethod();// call from class name. Static methods are hidden
+        C.staticMethod();
 
 
     }
