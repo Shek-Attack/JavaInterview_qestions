f7dcab18668f79006855adc4567a44b5556a7969
Noorl <ab427902@dal.ca>
2021-09-16 22:24:37 -0400

    practice branching

---
+++ src/git_branch/branch.java
@@ -0,0 +1,10 @@
+package git_branch;
+
+public class branch {
+    public static void main(String[] args) {
+
+        System.out.println("git branch name");
+        System.out.println("git checkout name");
+        System.out.println("git branch -b name");
+    }
+}
